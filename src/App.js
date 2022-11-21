383ebc6964a1aff92637e1f93ed9f9ebc9da23e8
ysy <ysy>
2022-11-21 13:38:29 +0900

    *

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";//순서대로 router 형식 선언(hashrouter도 있음. 단 #이 항상 붙는다.) as 컴포넌트 렌더링, route별로 렌더링, 컴포넌트 렌더링?
+import {BrowserRouter as Router, Routes, Switch, Route} from "react-router-dom";//순서대로 router 형식 선언(hashrouter도 있음. 단 #이 항상 붙는다.) as 컴포넌트 렌더링, route별로 렌더링, 컴포넌트 렌더링?
 import Home from './routes/Home';
 import Detail from './routes/Detail';
 
@@ -6,10 +6,10 @@ function App(){
     return(
         <Router basename={process.env.PUBLIC_URL}>
             <Switch>
-                <Route path="/Movie/:id" element={<Detail />}>
+                <Route path="/Movie/:id">
                     <Detail/>
                 </Route>
-                <Route path="/" element={<Home />} >
+                <Route path={`${process.env.PUBLIC_URL}/`} element={< Home />} >
                     <Home/>
                 </Route>
             </Switch>
