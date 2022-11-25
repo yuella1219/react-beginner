a54016ebb675e34a651cc512d161dd6d7f6a2bec
ysy <ysy>
2022-11-25 09:55:48 +0900

    n

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
-import { HashRouter, Router, Routes, Switch, Route} from "react-router-dom";//순서대로 router 형식 선언(hashrouter도 있음. 단 #이 항상 붙는다.) as 컴포넌트 렌더링, route별로 렌더링, 컴포넌트 렌더링?
+import { BrowserRouter as Router, Routes, Switch, Route} from "react-router-dom";//순서대로 router 형식 선언(hashrouter도 있음. 단 #이 항상 붙는다.) as 컴포넌트 렌더링, route별로 렌더링, 컴포넌트 렌더링?
 import Home from './routes/Home';
 import Detail from './routes/Detail';
 
 function App(){
     return(
-        <HashRouter basename={process.env.PUBLIC_URL}>
+        <Router basename={process.env.PUBLIC_URL}>
             <Switch>
-                <Route path="/Movie/:id">
+                <Route path="/movie/:id">
                     <Detail/>
                 </Route>
-                <Route path="/routes" element={<Home/>}>
+                <Route path="/" element={<Home/>}>
                     <Home/>
                 </Route>
             </Switch>
-        </HashRouter>
-
+        </Router>
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -7,9 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
-        <BrowserRouter basename={process.env.PUBLIC_URL}element={< App />}>
-            <App />
-        </BrowserRouter>
-    </React.StrictMode>
+    <BrowserRouter basename={process.env.PUBLIC_URL}element={< App />}>
+        <App />
+    </BrowserRouter>
 );
