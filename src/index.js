fb3e130fb356761dc730ab61977cfa6cbecfa44b
ysy <ysy>
2022-11-25 16:07:55 +0900

    practice save

--- src/index.js
+++ src/index.js
@@ -8,6 +8,6 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <BrowserRouter basename={process.env.PUBLIC_URL}element={< App />}>
-        <App />
+        <Practice />
     </BrowserRouter>
 );
--- src/practice.js
+++ src/practice.js
@@ -2,10 +2,31 @@ import { useEffect, useState } from "react"
 import React from 'react';
 import styles from "./css/index.module.css"
 
-function Practice(){
-  return(
-    <div></div>
-  )
+class Practice extends React.Component{
+
+  state = {
+    isLoading : true,
+    movies : []
+  }
+
+  componentDidMount(){
+    //컴포넌트가 렌더링 되기 전
+    console.log("react done!");
+  }
+  componentDidUpdate(){
+    //컴포넌트가 업데이트(리렌더링) 될 때
+    console.log('i just updated');
+  }
+  render(){
+    console.log('rendered');//컴포넌트를 렌더링 할 때 마다
+    const {isLoading} = this.state;
+    return (
+      <div>
+        <h1>{this.state.isLoading ? `Please Wait...` : null}</h1>
+      </div>
+    )
+    
+  }
 }
 
 export default Practice
