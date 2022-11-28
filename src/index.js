8abf4a7a308d71d004136cdc9560e52bd7093b30
ysy <ysy>
2022-11-28 15:39:22 +0900

    script Mode 추가

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter basename={process.env.PUBLIC_URL}element={< App />}>
-        <Practice />
-    </BrowserRouter>
+    <React.StrictMode basename={process.env.PUBLIC_URL}element={< App />}>
+        <App />
+    </React.StrictMode>
 );
